d210cb17a9d4c900a9a7a69b8ee5637480e37e5f
“sowmya-nittala” <sowmya.nittala@gmail.com>
2020-10-09 15:24:19 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -17,8 +17,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     recv = clientSocket.recv(1024).decode()
     print(recv)
 
-    if recv[:3] != '220':
-        print('220 reply not received from server.')
+    #if recv[:3] != '220':
+    #    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
