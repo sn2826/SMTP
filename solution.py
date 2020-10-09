a954020848f8189331e6f71d19ac57ff96bb4d9e
“sowmya-nittala” <sowmya.nittala@gmail.com>
2020-10-09 16:04:12 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -15,7 +15,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    print(recv)
+    #print(recv)
 
     #if recv[:3] != '220':
     #    print('220 reply not received from server.')
